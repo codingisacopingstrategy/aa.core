7ff3928828fd246963e6cb2e715d687df3e885b6
Michael Murtaugh <mm@automatist.org>
2011-07-05 09:22:03 +0200

    settings update

--- run/settings.py
+++ run/settings.py
@@ -31,7 +31,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Brussels'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -96,9 +96,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.markup',
-    'sorl.thumbnail',
-    'aabrowser',
-    'south',
+    'aacore',
 )
 
 
