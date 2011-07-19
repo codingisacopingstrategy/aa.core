0cb9755ce92a7a126c0a55e3d85f4017a6e40dfb
Alexandre Leray <alexandre@alexandreleray.com>
2011-07-19 16:19:06 +0200

    Moved plugins into plugins/

--- aacore/views.py
+++ aacore/views.py
@@ -21,7 +21,7 @@ from django.shortcuts import (render_to_response, get_object_or_404)
 from django.http import HttpResponse
 from django.template import RequestContext
 
-import sniffer
+from plugins import sniffer
 from models import *
 from rdfutils import *
 from utils import *
