80d00945cb3e4e5b1250ff9d8ffcdd9c946e164f
Alexandre Leray <alexandre@alexandreleray.com>
2011-11-16 15:23:47 +0100

    Fixed about attribute persistance

--- aacore/static/aacore/js/embed.js
+++ aacore/static/aacore/js/embed.js
@@ -70,7 +70,7 @@ $(document).bind("refresh", function (evt) {
             var href = $(ui.helper).attr("href");
             var s1 = $(this).closest(".section1");
             s1.attr("about", href);
-            post_styles(this, 'style');
+            post_styles(s1, 'style');
             // console.log("href", href, this);
             // post_styles(s1);
             // TODO:
