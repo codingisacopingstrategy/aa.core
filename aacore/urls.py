98c3170032f78637b2e55703ed4027940bba5a58
Alexandre Leray <alexandre@alexandreleray.com>
2011-11-16 12:09:18 +0100

    Reverted diff url gone mysteriously...

--- aacore/urls.py
+++ aacore/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('aacore.views',
     url(r'^pages/(?P<slug>[^/]+)/$', 'page_detail', {}, name='aa-page-detail'),
     url(r'^pages/(?P<slug>[^/]+)/edit/$', 'page_edit', {}, name='aa-page-edit'),
     url(r'^pages/(?P<slug>[-\w]+)/history/$', 'page_history', {}, name='aa-page-history'),
+    url(r'^pages/(?P<slug>[-\w]+)/diff/$', 'page_diff', {}, name='aa-page-diff'),
 
     ### Export
     url(r'^pages/(?P<slug>[^/]+)/(?P<section>\d+)/$', 'annotation_export', {}, name='aa-annotation-export'),
