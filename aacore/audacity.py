ee5c327ffdf7c85fdcb76b17951018a6111f41cc
Alexandre Leray <alexandre@alexandreleray.com>
2011-12-05 03:10:14 +0100

    Added an implicit timecode option in audacity_to_srt

--- aacore/audacity.py
+++ aacore/audacity.py
@@ -1,13 +1,42 @@
 from timecode import timecode_fromsecs
 
 
-def audacity_to_srt(data):
+def audacity_to_srt(data, explicit=False):
     """
     >>> data = '''90,610022  90,610022   first section
     ... 345,271874  345,271874 second section'''
-    >>> audacity_to_srt(data)
+    >>> print(audacity_to_srt(data).strip())
+    00:01:30.610 --> 
+    <BLANKLINE>
+    first section
+    <BLANKLINE>
+    00:05:45.272 --> 
+    <BLANKLINE>
+    second section
+
+    >>> data = '''90,610022  345,271874   first section
+    ... 345,271874  512,573912 second section'''
+    >>> print(audacity_to_srt(data).strip())
+    00:01:30.610 --> 
+    <BLANKLINE>
+    first section
+    <BLANKLINE>
+    00:05:45.272 --> 00:08:32.574
+    <BLANKLINE>
+    second section
+
+    >>> data = '''90,610022  345,271874   first section
+    ... 345,271874  512,573912 second section'''
+    >>> print(audacity_to_srt(data, explicit=True).strip())
+    00:01:30.610 --> 00:05:45.272
+    <BLANKLINE>
+    first section
+    <BLANKLINE>
+    00:05:45.272 --> 00:08:32.574
+    <BLANKLINE>
+    second section
     """
-    srt = ""
+    stack = []
     for line in data.splitlines():
         (start, end, body) = tuple(line.split(None, 2))
 
@@ -23,23 +52,23 @@ def audacity_to_srt(data):
         if end == start:
             end = ""
 
-        #body = body.decode('unicode-escape')
+        if not explicit:
+            # Deletes previous end time if equal to actual start time
+            if len(stack) and stack[-1]['end'] == start:
+                stack[-1]['end'] = ""
+
         body = body.replace(r'\n', '\n')
 
-        srt += "{} --> {}\n\n{}\n\n".format(start, end, body)
-    return srt
+        stack.append({'start': start, 'end': end, 'body': body})
+
+    template = "{e[start]} --> {e[end]}\n\n{e[body]}\n\n"
+    return "".join([template.format(e=e) for e in stack])
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-    import codecs
-    #f = codecs.open('/tmp/marqueurs.txt', mode='r', encoding='unicode-escape')
-    f = open('/tmp/marqueurs.txt', 'r')
-    data = f.read()
-    f.close()
-    #print(data)
-    print(audacity_to_srt(data))
-    #f = codecs.open('/tmp/marqueurs.srt', mode='w', encoding='utf-8')
-    #f.write(audacity_to_srt(data))
+    #f = open('/tmp/marqueurs.txt', 'r')
+    #data = f.read()
     #f.close()
+    #print(audacity_to_srt(data))
