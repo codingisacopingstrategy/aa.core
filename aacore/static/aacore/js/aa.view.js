codingisacopingstrategy/aa.core

commit bc84e05a98a126523fd1c2540eb54a59b8ec2405

Fixed lazy loading of embeds and associated scripts

aacore/static/aacore/js/aa.view.js
@@ -228,7 +228,7 @@
                         show: function (elt) {
                             $(elt).addClass("active")
                                 .closest('section.section1')
-                                   .find('div.wrapper:first')
+                                   .find('div.wrapper')
                                         .autoscrollable("scrollto", elt);
                         },
                         hide: function (elt) {
@@ -467,33 +467,35 @@
                                 // - extra_css: loads extra link rel="stylesheet"
                                 // - extra_js: load extra script
                                 // - script: extra javascript code to execute
-                                for (var i = 0; i < data.extra_css.length; i++) {
-                                    var href = data.extra_css[i];
-                                    //if (!$('link[href="' + href + '"]').length) {
-                                    $('<link rel="stylesheet" type="text/css" media="screen">')
-                                        .attr("href", href)
-                                        .appendTo($('head'));
-                                    //};
-                                };
-                                for (var i = 0; i < data.extra_js.length; i++) {
-                                    var src = data.extra_js[i];
-                                    $('<script>').attr("src", src).appendTo($('head'));
-                                };
 
-                                var new_content = $(data.content);
-                                $(that).replaceWith(new_content);
+                                var toGo = data.extra_css.length + data.extra_js.length;
 
-                                // FIXME: This is a hack to make sure the html is load before executing the script.
-                                //new_content.ready(function() {
-                                setTimeout(function() {
-                                    var script_elt = document.createElement('script');
-                                    script_elt.text = data.script
-                                    var head = document.getElementsByTagName('body')[0].appendChild(script_elt);
-                                    //$(script_elt).appendTo($('head'));
-                                }, 1000);
-                                //});
+                                function doit () {
+                                    if (toGo == 0) {
+                                        var html = data.content + '<script>' + data.script + '</script>';
+                                        $(that).replaceWith($(html)).trigger("refresh");
+                                    } else {
+                                        toGo -= 1;
+                                    }
+                                }
 
-                                $(new_content).trigger("refresh");
+                                $.each(data.extra_css, function(index, value) { 
+                                    $.getCSS(value, function() {
+                                        //console.log('loaded css: ' + value);   
+                                        toGo -= 0;
+                                        doit();
+                                    });
+                                });
+
+                                $.each(data.extra_js, function(index, value) { 
+                                    $.getScript(value, function() {
+                                        //console.log('loaded js: ' + value);   
+                                        toGo -= 0;
+                                        doit();
+                                    });
+                                });
+
+                                doit();
                             } else {
                                 if (data.content) {
                                     $(that).html(data.content);

aacore/templates/aacore/page_detail.html
@@ -21,6 +21,7 @@
 <script src="{{ STATIC_URL }}aacore/js/aa.browser.js"></script>
 <script src="{{ STATIC_URL }}aacore/js/embed.js"></script>
 <script src="{{ STATIC_URL }}lib/iscroll/src/iscroll.js"></script>
+<script src="{{ STATIC_URL }}lib/jquery.getCSS.min.js"></script>
 
 <link rel="stylesheet" href="{{ STATIC_URL }}aacore/css/aa.styles.play.css" media="screen"/>
 

commit 2485a5c491a75ee89e9497d607407eb0d08f4231

Fixed a bug with autoscrollable introduced in bc84e05a98a

aacore/static/aacore/js/aa.view.js
@@ -228,7 +228,7 @@
                         show: function (elt) {
                             $(elt).addClass("active")
                                 .closest('section.section1')
-                                   .find('div.wrapper')
+                                   .find('div.wrapper:first')
                                         .autoscrollable("scrollto", elt);
                         },
                         hide: function (elt) {
@@ -467,17 +467,11 @@
                                 // - extra_css: loads extra link rel="stylesheet"
                                 // - extra_js: load extra script
                                 // - script: extra javascript code to execute
-
                                 var toGo = data.extra_css.length + data.extra_js.length;
+                                var html = $(data.content);
 
-                                function doit () {
-                                    if (toGo == 0) {
-                                        var html = data.content + '<script>' + data.script + '</script>';
-                                        $(that).replaceWith($(html)).trigger("refresh");
-                                    } else {
-                                        toGo -= 1;
-                                    }
-                                }
+                                $(that).replaceWith(html);
+                                html.trigger("refresh");
 
                                 $.each(data.extra_css, function(index, value) { 
                                     $.getCSS(value, function() {
@@ -494,6 +488,14 @@
                                         doit();
                                     });
                                 });
+
+                                function doit () {
+                                    if (toGo == 0) {
+                                        $.globalEval(data.script);
+                                    } else {
+                                        toGo -= 1;
+                                    }
+                                }
 
                                 doit();
                             } else {