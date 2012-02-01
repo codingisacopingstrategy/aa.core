bb83ead9dc73d70a56bb4ef287b902c0cb0a5606
Alexandre Leray <alexandre@alexandreleray.com>
2012-02-01 16:27:34 +0100

    Improved mdx del ins extension

--- aacore/mdx/mdx_del_ins.py
+++ aacore/mdx/mdx_del_ins.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 
 '''
@@ -20,49 +20,28 @@ Basic usage:
 '''
 
 
-import markdown
 import re
+import markdown
+from markdown.inlinepatterns import SimpleTagPattern
 
 
-DEL_INS_RE = r"(?P<type>\~\~|\+\+)(?P<content>.+?)(\~\~|\+\+)"
+DEL_RE = r"(\~\~)(.+?)(\~\~)"
+INS_RE = r"(\+\+)(.+?)(\+\+)"
 
 
-class DelInsExtension(markdown.Extension):
+class DelInsExtension(markdown.extensions.Extension):
+    """ Add smart_emphasis extension to Markdown class."""
+
     def extendMarkdown(self, md, md_globals):
-        self.md = md
-    
-        # append to end of inline patterns
-        pat = DelInsPattern(self.config, md)
-        md.inlinePatterns.add('delins', pat, "<not_strong")
+        """ Modify inline patterns. """
+        md.inlinePatterns.add('del', SimpleTagPattern(DEL_RE, 'del'), '<not_strong')
+        md.inlinePatterns.add('ins', SimpleTagPattern(INS_RE, 'ins'), '<not_strong')
 
 
-class DelInsPattern(markdown.inlinepatterns.Pattern):
-    def __init__(self, config, md=None):
-        markdown.inlinepatterns.Pattern.__init__(self, '', md)
-        self.compiled_re = re.compile(r'^(.*?)' + DEL_INS_RE + r'(.*?)$')
-        self.config = config
-
-    def getCompiledRegExp (self):
-        return self.compiled_re
-  
-    def handleMatch(self, m):
-        """ Returns etree """
-        d = m.groupdict()
-        content = d.get("content")
-        type_ = d.get("type")
-        if type_ == "~~":
-            elt = markdown.util.etree.Element('del')
-        elif type_ == "++":
-            elt = markdown.util.etree.Element('ins')
-        elt.text = content
-        return elt
-
-
-def makeExtension(configs={}) :
-    return DelInsExtension(configs=configs)
+def makeExtension(configs={}):
+    return DelInsExtension(configs=dict(configs))
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-
